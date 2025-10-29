41aacf77e739612e06e7f534f6f6b28e563150dc
ParkSeongWon <qkrtjddnjs85@gmail.com>
2025-10-29 12:04:47 +0900

    first commit

---
+++ Core/Inc/rc_control.h
@@ -0,0 +1,13 @@
+#ifndef INC_RC_CONTROL_H_
+#define INC_RC_CONTROL_H_
+
+#include "stm32f4xx_hal.h"
+
+void moveForward();
+void moveBack();
+void moveLeft();
+void moveRight();
+
+
+
+#endif /* INC_RC_CONTROL_H_ */
